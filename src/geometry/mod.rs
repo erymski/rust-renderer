02890f4c40d04c508263e0f4ed948746eda04ab8
Eugene Rymski <erymski@gmail.com>
2026-08-29 10:02:45 +0200

    Remove 2D stuff

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,17 +1,10 @@
-pub(crate) mod circle;
 pub(crate) mod colors;
 pub(crate) mod hit3d;
 pub(crate) mod ray;
-pub(crate) mod rect2d;
-pub(crate) mod scene2d;
 pub(crate) mod scene3d;
 pub(crate) mod sphere;
 pub(crate) mod vec2;
 pub(crate) mod vec3;
-
-pub(crate) trait Hittable2d {
-    fn hit(&self, pt: &Vec2) -> Option<Color>;
-}
 
 pub(crate) trait Hittable3d {
     fn hit(&self, ray: &Ray) -> Option<Hit3d>;
@@ -24,5 +17,4 @@ pub use hit3d::Hit3d;
 pub use ray::Ray;
 pub(crate) use scene3d::Scene3d;
 pub use sphere::Sphere;
-pub use vec2::Vec2;
 pub use vec3::{Color, Point3, Vec3};
