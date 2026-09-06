1044f8837c6c9e1b79ffd50b4a2341cb34334068
Eugene Rymski <erymski@gmail.com>
2026-09-06 18:06:49 +0200

    Added color clamping

--- src/geometry/tone_mapping.rs
+++ src/geometry/tone_mapping.rs
@@ -6,3 +6,25 @@ pub type ToneMapper = fn(&Color) -> Color;
 pub fn color_identity(color: &Color) -> Color {
     color.clone()
 }
+
+#[allow(dead_code)]
+pub fn color_clamp(color: &Color) -> Color {
+    Color {
+        x: color.x.max(0.).min(1.0),
+        y: color.y.max(0.).min(1.0),
+        z: color.z.max(0.).min(1.0),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::test_utils::assert_vec3_eq;
+
+    use super::*;
+
+    #[test]
+    fn clamp_check() {
+        let color = Color::new(-5., 12., 0.);
+        assert_vec3_eq(&color_clamp(&color), &Color::new(0., 1., 0.));
+    }
+}
