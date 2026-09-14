64383be4ec7f53ba2e187a724233a380dc03b8ae
Eugene Rymski <erymski@gmail.com>
2026-09-15 00:21:12 +0200

    Primitive shadowing

--- src/renderer.rs
+++ src/renderer.rs
@@ -9,12 +9,20 @@ pub(crate) fn hit_for_scene(hit: &Hit, scene: &Scene) -> Color {
     };
 
     for light in scene.lights() {
-        let diffuse = light.direction.dot(&hit.normal).neg();
-        let diffuse_light = light.color.scale(diffuse);
+        // check if the light is reachable
+        let ray_to_light = Ray::new(hit.point, light.direction.scale(-1.));
+        match scene.intersect(&ray_to_light) {
+            None => {
+                // nothing between light and the object
+                let diffuse = light.direction.dot(&hit.normal).neg();
+                let diffuse_light = light.color.scale(diffuse);
 
-        let colored_hit = hit.color.mult(&diffuse_light);
+                let colored_hit = hit.color.mult(&diffuse_light);
 
-        result.add_mut(&colored_hit);
+                result.add_mut(&colored_hit);
+            }
+            Some(_) => continue,
+        };
     }
     result
 }
