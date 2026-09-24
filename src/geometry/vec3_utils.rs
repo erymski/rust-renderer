b7e3e7e3af00384bfed160156b3e430b38337172
Eugene Rymski <erymski@gmail.com>
2026-09-25 00:01:45 +0200

    Reflect implementation

--- src/geometry/vec3_utils.rs
+++ src/geometry/vec3_utils.rs
@@ -2,6 +2,7 @@ use crate::geometry::DEFAULT_EPSILON;
 use crate::geometry::{V, Vec3};
 use rand::random_range;
 
+/// generate random unit vector
 pub fn random_unit() -> Vec3 {
     loop {
         let mut res = V(
@@ -19,11 +20,17 @@ pub fn random_unit() -> Vec3 {
     }
 }
 
+/// reflect v against n (normal)
+pub fn reflect(v: Vec3, n: Vec3) -> Vec3 {
+    let v_projected_to_n = n * v.dot(&n);
+    v - v_projected_to_n * 2.0
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
-    use crate::geometry::test_utils::{assert_approx_eq, assert_vec3_neq};
+    use crate::geometry::test_utils::{assert_approx_eq, assert_vec3_eq, assert_vec3_neq};
 
     #[test]
     fn vec3_random() {
@@ -33,5 +40,15 @@ mod tests {
         assert_approx_eq(v2.length_squared(), 1.0);
 
         assert_vec3_neq(&v1, &v2);
+    }
+
+    #[test]
+    fn vec3_reflection() {
+        let v = V(1.0, -1.0, 0.0);
+        let n = V(0.0, 1.0, 0.0);
+
+        let r = reflect(v, n);
+
+        assert_vec3_eq(&r, &V(1.0, 1.0, 0.0));
     }
 }
